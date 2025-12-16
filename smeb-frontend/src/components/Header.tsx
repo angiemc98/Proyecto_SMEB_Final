3a69c61806652bed93feca86b7ca4d860add280f
angiemc98 <molinanatalia@hotmail.com>
2025-12-16 16:54:19 -0500

    feat: correción del header

--- smeb-frontend/src/components/Header.tsx
+++ smeb-frontend/src/components/Header.tsx
@@ -9,9 +9,7 @@ export default function Header() {
             <h1 className="text-lg font-semibold tracking-wide">SMEB - Gestión Biomédica</h1>
             <div className="flex items-center gap-4">
                 <div className="w-6 h-6 bg-red-500 rounded-full text-xs flex items-center justify-center font-bold">3</div>
-                <div className="flex items-center bg-white text-[#1565c0] px-3 py-1 rounded-lg shadow-sm">TC</div>
-                    <span className="text-sm font-medium">Carlos Perez</span>
-                    <Button onClick={() => window.open("auth/login", "_self")} className="bg-white text-[#0d47a1] px-3 py-1 rounded-lg shadow-sm hover:bg-red">Cerrar</Button>
+                    <Button onClick={() => window.open("/auth/login", "_self")} className="bg-white text-[#0d47a1] px-3 py-1 rounded-full shadow-sm font medium hover:bg-red">CP</Button>
             </div>
         </header>
     );
